778326c81a468f2ddb8d31660220da6ec3fd7aeb
Alex Varganov <67458241+slaycid@users.noreply.github.com>
2022-06-20 20:08:37 +0500

    Update run.js

--- camera/run.js
+++ camera/run.js
@@ -3,11 +3,11 @@ Runner
 */
 
 // Both available renderers are imported
-import textRenderer from './core/textrenderer.js'
-import canvasRenderer from './core/canvasrenderer.js'
-import FPS from './core/fps.js'
-import storage from './core/storage.js'
-import RUNNER_VERSION from './core/version.js'
+import textRenderer from 'textrenderer.js'
+import canvasRenderer from 'canvasrenderer.js'
+import FPS from 'fps.js'
+import storage from 'storage.js'
+import RUNNER_VERSION from 'version.js'
 
 export { RUNNER_VERSION }
 
